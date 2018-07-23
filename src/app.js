0249ea8ad6e3ccf427b31622a48814bb4de1a08f
spacedgatito <miluska.gonzalezar@gmail.com>
2018-07-23 06:10:08 -0500

    Valida inputs de email y password. Agrega login con Facebook

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
+// Inicializando Firebase
+const config = {
+  apiKey: "AIzaSyDebuWMYcGSWAk5x7YGxciGKStz56KOHlY",
+  authDomain: "social-network-salud.firebaseapp.com",
+  databaseURL: "https://social-network-salud.firebaseio.com",
+  projectId: "social-network-salud",
+  storageBucket: "social-network-salud.appspot.com",
+  messagingSenderId: "628278045322"
+};
+firebase.initializeApp(config);
 // Declarando variables del form
-const name = document.getElementById('name');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const signup = document.getElementById('signup');
-const form = document.getElementById('form');
-// Evento de registro
-signup.addEventListener('click', () => {
-  username = name.value;
+const signUp = document.getElementById('sign-up');
+const signUpForm = document.getElementById('sign-up-form');
+const fbBtn = document.getElementById('fbBtn');
+// Crear usuario con email y contraseña
+const emailSignUp = () => {
   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
     .then(user => {
+      console.log(password.value)
       //Verificando que se registró el usuario
       console.log('nuevo usuario registrado!');
     })
@@ -17,12 +28,24 @@ signup.addEventListener('click', () => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorCode, errorMessage);
+      console.log(password.value)
     });
-    // reset() limpia el form
-    form.reset();
-    console.log(username);
-    alert(`¡Gracias por registrarte ${username}!`);
-});
+  // reset() limpia el form
+  signUpForm.reset();
+  console.log(name);
+  alert(`¡Gracias por registrarte ${name}!`);
+}
+// Validar inputs
+const validate = () => {
+  if (/^\w+([\.-]?\w+)*@(([a-zA-Z0-9\-])+\.)+([a-zA-Z]{2,4})+$/.test(email.value) &&
+    /^[a-zA-Z\d]{6,}$/.test(password.value) &&
+    /^[a-z]{3,}$/i.test(username.value)) {
+    const name = username.value;
+    emailSignUp()
+  }
+}
+// Evento de registro
+signUp.addEventListener('click', validate);
 // Verificando estado (logueado / no logueado) del usuario
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
@@ -33,3 +56,29 @@ firebase.auth().onAuthStateChanged(user => {
     console.log(user, 'is signed out')
   }
 });
+// Creando instancia del objeto proveedor de Facebook
+const fbProvider = new firebase.auth.FacebookAuthProvider();
+// Log-in con Facebook
+const fbLogIn = () => {
+  firebase.auth().signInWithPopup(fbProvider)
+    .then(result => {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      alert('Habemus Facebook login')
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(error)
+    });
+}
+// Evento log-in con Facebook
+fbBtn.addEventListener('click', fbLogIn)
