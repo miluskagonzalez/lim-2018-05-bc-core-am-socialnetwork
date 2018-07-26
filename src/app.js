a3d21ad72afd84204a150463142b4af20d68524a
spacedgatito <miluska.gonzalezar@gmail.com>
2018-07-26 02:01:44 -0500

    Redirecciona usuario logueado a Home.html.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-// Inicializando Firebase
-const config = {
-  apiKey: "AIzaSyDebuWMYcGSWAk5x7YGxciGKStz56KOHlY",
-  authDomain: "social-network-salud.firebaseapp.com",
-  databaseURL: "https://social-network-salud.firebaseio.com",
-  projectId: "social-network-salud",
-  storageBucket: "social-network-salud.appspot.com",
-  messagingSenderId: "628278045322"
-};
-firebase.initializeApp(config);
 // Declarando variables del form de registro
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -18,138 +8,37 @@ const signUpForm = document.getElementById('sign-up-form');
 const userEmail = document.getElementById('user-email');
 const userPassword = document.getElementById('user-password');
 const btnSignIn = document.getElementById('sign-in');
-const btnSignOut = document.getElementById('sign-out');
 // Botones de registro con proveedor
 const fbBtn = document.getElementById('fbBtn');
 const btnGoogle = document.getElementById('btnGoogle');
-// Creando usuario con email y contraseña
-const emailSignUp = () => {
-  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-    .then(user => {
-      console.log(password.value);
-      //Verificando que se registró el usuario
-      console.log('nuevo usuario registrado!');
-    })
-    .catch(error => {
-      // Mostrando error en consola
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-      console.log(password.value);
-    });
-  // reset() limpia el form
-  signUpForm.reset();
-  console.log(name);
-  alert(`¡Gracias por registrarte ${name}!`);
-}
-// Validando inputs
+
+// Validando inputs y registrando usuario
 const validate = () => {
   if (/^\w+([\.-]?\w+)*@(([a-zA-Z0-9\-])+\.)+([a-zA-Z]{2,4})+$/.test(email.value) &&
     /^[a-zA-Z\d]{6,}$/.test(password.value) &&
     /^[a-z]{3,}$/i.test(username.value)) {
-    const name = username.value;
-    emailSignUp();
+    emailSignUp(username.value, email.value, password.value);
   }
 }
-// Sign-in con email y password
-const emailSignIn = () => {
-    // Consiguiendo valor de e-mail y contraseña
-    const email = userEmail.value;
-    const pass = userPassword.value;
-    // const auth = firebase.auth();
-    // Sign in
-    const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
-      // implementar
-      promise.then(user => {
-        console.log(user);
-      })
-      .catch(e => console.log('Usuario no existente, Registrarse'))
-}
-// Sign-in con Facebook
-const fbSignIn = () => {
-  // Creando instancia del objeto proveedor de Facebook
-  const fbProvider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(fbProvider)
-    .then(result => {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      console.log('result', result);
-      const token = result.credential.accessToken;
-      console.log('token', token);
-      // The signed-in user info.
-      const user = result.user;
-      console.log('user', user);
-      // ...
-      alert('Habemus Facebook signin')
-    }).catch(function (error) {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      // ...
-      console.log(error)
-    });
-}
-// Sign-in con Google
-googleSignIn = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-    .then(result => {
-      console.log('Sesion con google');
-      console.log(result);
-    }).catch(error => {
-      console.log(error.code);
-      console.log(error.message);
-      // The email of the user's account used.
-      console.log(error.email);
-      // The firebase.auth.AuthCredential type that was used.
-      console.log(error.credential);
-      // ...
-    })
-};
-
-// Estado del usuario actual
-firebase.auth().onAuthStateChanged(user => {
-  // Usuario está logueado
-  console.log(user);
-  if (user) {
-    // btnSignOut.classList.remove('hide');
-  } else {
-    // btnSignOut.classList.add('hide');
-    // Usuario no está logueado
-    console.log(user, 'is signed out');
-  }
-});
-// Sign-out del usuario
-signOut = () => {
-    firebase.auth().signOut().then(() => {
-      console.log('Signed Out');
-    }, (error) => {
-      console.error('Sign Out Error', error);
-    });
-};
 // Evento de validación y registro
 signUp.addEventListener('click', validate);
 // Evento sign-in con correo y contraseña de usuario ya registrado
-btnSignIn.addEventListener('click', emailSignIn);
+btnSignIn.addEventListener('click', () => emailSignIn(userEmail.value, userPassword.value));
 // Evento sign-in con Facebook
 fbBtn.addEventListener('click', fbSignIn)
 // Evento sign-in con Google
 btnGoogle.addEventListener('click', googleSignIn);
-// Evento sign-out
-// btnSignOut.addEventListener('click', signOut);
 // Cuando el tamaño de la pantalla cambia, amplia o reduce las opciones de login
 // La propiedad innerWidth tiene una falla de aproximadamente 70 px. 
 // quedaría averiguar si hay otra forma de hacer esta parte, pero por ahora funciona :)
 window.addEventListener('resize', event => {
   if (event.target.innerWidth >= 714) {
-    console.log(event.target.innerWidth)
-    document.getElementById('button-login-form').classList.add('no-display')
-    document.getElementById('form-login').classList.remove('no-display')
+    document.getElementById('button-login-form').classList.add('no-display');
+    document.getElementById('form-login').classList.remove('no-display');
   } else {
-    document.getElementById('form-login').classList.add('no-display')
-    document.getElementById('button-login-form').classList.remove('no-display')
+    document.getElementById('form-login').classList.add('no-display');
+    document.getElementById('button-login-form').classList.remove('no-display');
   }
 });
+// Evento sign-out
+//btnSignOut.addEventListener('click', signOut);
---
+++ src/auth.js
@@ -0,0 +1,106 @@
+// Inicializando Firebase
+const config = {
+  apiKey: "AIzaSyDebuWMYcGSWAk5x7YGxciGKStz56KOHlY",
+  authDomain: "social-network-salud.firebaseapp.com",
+  databaseURL: "https://social-network-salud.firebaseio.com",
+  projectId: "social-network-salud",
+  storageBucket: "social-network-salud.appspot.com",
+  messagingSenderId: "628278045322"
+};
+firebase.initializeApp(config);
+
+// Creando usuario con email y contraseña
+window.emailSignUp = (name, email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(user => {
+      console.log(password);
+      //Verificando que se registró el usuario
+      console.log('nuevo usuario registrado!');
+      location.href = 'home.html'
+    })
+    .catch(error => {
+      // Mostrando error en consola
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      console.log(password.value);
+    });
+  // reset() limpia el form
+  signUpForm.reset();
+  console.log(name);
+  alert(`¡Gracias por registrarte ${name}!`);
+}
+// Sign-in con email y password
+window.emailSignIn = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => {
+      console.log(user);
+      location.href = 'home.html'
+    })
+    .catch(e => console.log('Usuario no existente, Registrarse'))
+}
+// Sign-in con Facebook
+window.fbSignIn = () => {
+  // Creando instancia del objeto proveedor de Facebook
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(fbProvider)
+    .then(result => {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      console.log('result', result);
+      const token = result.credential.accessToken;
+      console.log('token', token);
+      // The signed-in user info.
+      const user = result.user;
+      console.log('user', user);
+      // ...
+      alert('Habemus Facebook signin')
+      location.href = 'home.html'
+    }).catch(function (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+      console.log(error);
+    });
+};
+// Sign-in con Google
+window.googleSignIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then(result => {
+      console.log('Sesion con google');
+      console.log(result);
+      location.href = 'home.html'
+    }).catch(error => {
+      console.log(error.code);
+      console.log(error.message);
+      // The email of the user's account used.
+      console.log(error.email);
+      // The firebase.auth.AuthCredential type that was used.
+      console.log(error.credential);
+      // ...
+    })
+};
+// Sign-out del usuario
+window.signOut = () => {
+  firebase.auth().signOut().then(() => {
+    console.log('Signed Out');
+    location.href = 'index.html'
+  }, (error) => {
+    console.error('Sign Out Error', error);
+  });
+};
+// Estado del usuario actual
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    // Usuario está logueado
+  console.log(user);
+  } else {
+    // Usuario no está logueado
+    console.log(user, 'is signed out');
+  }
+});
