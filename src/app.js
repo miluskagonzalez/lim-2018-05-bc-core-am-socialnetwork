d93c1ff50764461ce3585ca65f5c577ba05f5856
spacedgatito <miluska.gonzalezar@gmail.com>
2018-07-27 17:43:02 -0500

    cambia nombre de archivo auth.js por firebase.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const btnGoogle = document.getElementById('btnGoogle');
 const validate = () => {
   if (/^\w+([\.-]?\w+)*@(([a-zA-Z0-9\-])+\.)+([a-zA-Z]{2,4})+$/.test(email.value) &&
     /^[ña-zA-Z\d]{6,}$/.test(password.value) &&
-    /^[ña-z ]{2,20}$/i.test(username.value)) {
+    /^[ña-z ]{2,30}$/i.test(username.value)) {
     emailSignUp(username.value, email.value, password.value);
   }
 }
